2f0a2c4aed536e95bca8b834ec1a12fba660be48
HongyiHao-SXIT <Lanyi_adict@outlook.com>
2025-08-07 23:03:10 +0800

    init

---
+++ src/main/java/com/gugugu/wolfmomo/WolfmomoApplication.java
@@ -0,0 +1,13 @@
+package com.gugugu.wolfmomo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WolfmomoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WolfmomoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/gugugu/wolfmomo/pojo/TransactionRecord.java
@@ -0,0 +1,33 @@
+package com.gugugu.wolfmomo.pojo;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TransactionRecord {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    private User user;
+
+    @Column(nullable = false)
+    private BigDecimal amount;
+
+    @Column(nullable = false)
+    private String type;
+
+    private String description;
+
+    private LocalDate timestamp;
+}
