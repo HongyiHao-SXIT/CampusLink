20d4c8dd100c535f8e89075f9f5d8aebe0e4ce12
HongyiHao-SXIT <Lanyi_adict@outlook.com>
2025-10-03 19:21:21 +0800

    add back end

---
+++ campuslink_back_end/src/main/java/com/sxit/campus/campuslink_back_end/CampuslinkBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.sxit.campus.campuslink_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampuslinkBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampuslinkBackEndApplication.class, args);
+	}
+
+}
---
+++ campuslink_back_end/src/test/java/com/sxit/campus/campuslink_back_end/CampuslinkBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sxit.campus.campuslink_back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampuslinkBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
